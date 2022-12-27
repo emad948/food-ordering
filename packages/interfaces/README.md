75f86b0ffde9848f8b6e2e2390192b47780cbec4
emadalmsouti <almsouti.e@gmail.com>
2022-12-27 01:44:38 +0100

    Initial commit: Set up the project structure and added the necessary files and dependencies.

---
+++ packages/interfaces/README.md
@@ -0,0 +1,2 @@
+# Interfaces
+This package contains shared interfaces which can be used in other packages.
---
+++ packages/store-front/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <p>emad</p>
+    </div>
+  );
+}
+
+export default App;
